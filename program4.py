8d2b2c36d2064f1160c3af9822db7c0decf4c9c1
dhavan2905-cpu <dhavan2905@gmail.com>
2026-01-12 05:54:27 +0000

    p1 compited

--- program4.py
+++ program4.py
@@ -1,1 +1,19 @@
+def factorial(n):
+ if n==0 or 1:
+  return 1\
+  #recursive case:n!=n*(n-1)!
+ else:
+  return n*(n-1)
+number=int(input("enter a number:"))
+if number<0:
+ 
+    print("/nError:factorial is not defined for negative numbers")
+else:
+        result=factorial(number)
+        print(f"\nThe factorial of{number} is {result}")
+
+
+
+
+
 
